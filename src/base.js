109229ebef12568b5432c74f9bb77b0d14d6ef34
damiecode <damidigi@yahoo.com>
2020-01-25 13:16:51 +0100

    Add content to pagewith JavaScript

--- src/base.js
+++ src/base.js
@@ -1,5 +1,15 @@
 const elements = {
-  content: document.querySelector(".content")
+  content: document.querySelector(".content"),
 
-  header: 
+  header: document.createElement("section"),
+  container: document.createElement("div"),
+  navbar: document.createElement("div"),
+
+  main: document.createElement("main"),
+    gallery: document.createElement("section"),
+    menu: document.createElement("section"),
+    contact: document.createElement("section"),
+    footer: document.createElement("footer"),
+    copyright: document.createElement("section")
 }
+
