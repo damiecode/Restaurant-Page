899df869aaa0e96aec5dfe5f28d1ec0978813167
damiecode <damidigi@yahoo.com>
2020-01-28 18:22:37 +0100

    Update header and home page

--- src/modules/sections/base.js
+++ src/modules/sections/base.js
@@ -5,21 +5,51 @@ const base = () => {
   container.classList.add('container')
   const navbarHeader = document.createElement('div')
   navbarHeader.classList.add('navbar-header')
+  const button = document.createElement('button')
+  button.classList.add('navbar-toggle')
+  button.setAttribute('data-toggle', 'collapse')
+  // $('[data-toggle="collapse"]').collapse()
+  button.dataset.target = '.navbar-collapse'
+  const brand = document.createElement('a')
+  brand.classList.add('navbar-brand')
+  brand.innerText = 'CATERLY'
+
+  const navDiv = document.createElement('div')
+  navDiv.classList.add('collapse','navbar-collapse')
+  const list = document.createElement('ul')
+  list.classList.add('nav','navbar-nav','navbar-right')
+
+  const toggle = ['', '', '']
+  toggle.forEach((el) => {
+    let span = document.createElement('span')
+    span.classList.add('icon', 'icon-bar')
+    button.appendChild(span)
+  })
 
   const navArray = ['HOME', 'FOOD GALLERY', 'SPECIAL MENU', 'CONTACT'];
   const anchorArray = [];
 
   for (let i = 0; i < 4; i += 1) {
     const a = document.createElement('a');
+    const li = document.createElement('li');
     if (i !== 0) a.setAttribute('href', `#page${i}`);
     a.classList.add('smoothScroll');
     a.innerHTML = navArray[i];
-    anchorArray.push(a);
+    li.appendChild(a)
+    anchorArray.push(li);
   }
 
-  navbarHeader.append(...anchorArray);
+  navbarHeader.appendChild(button);
+
+  navbarHeader.appendChild(brand)
 
   container.appendChild(navbarHeader);
+
+  container.appendChild(navDiv)
+
+  list.append(...anchorArray)
+
+  navDiv.appendChild(list)
 
   header.appendChild(container);
 
